bc647ffd1650880bfa3f6e6e7f369f16d8973dfc
isra <=>
2025-05-28 20:38:08 +0300

    update the roomdetails form

--- Otel yönetim Sistemi/Forms/Roomdetails.Designer.cs
+++ Otel yönetim Sistemi/Forms/Roomdetails.Designer.cs
@@ -137,7 +137,7 @@
             this.groupBox1.Size = new System.Drawing.Size(314, 448);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Update info";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // label1
