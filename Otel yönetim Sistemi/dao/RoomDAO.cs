da46d3ad1faacae31edd20e1b3627f38add37303
muhegebi20 <muhegebi2021@gmail.com>
2025-05-28 11:17:33 +0300

    added room update get specific room and delete functionalities

--- Otel yönetim Sistemi/dao/RoomDAO.cs
+++ Otel yönetim Sistemi/dao/RoomDAO.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using Otel_yonetim_Sistemi.Model;
+using Otel_yonetim_Sistemi.dao;
 using Otel_yönetim_Sistemi.Model;
 
 namespace Otel_yönetim_Sistemi.dao
@@ -21,5 +23,35 @@ namespace Otel_yönetim_Sistemi.dao
             var rooms = _repository.GetCollection<Room>("rooms");
             rooms.InsertOne(room);
         }
+        public void UpdateRoom(Room room) 
+        {
+            var rooms = _repository.GetCollection<Room>("rooms");
+            var filter = Builders<Room>.Filter.Eq(r => r.Id, room.Id);
+            rooms.ReplaceOne(filter, room);
+        }
+        public void DeleteRoom(ObjectId roomId)
+        {
+            var rooms = _repository.GetCollection<Room>("rooms");
+            var filter = Builders<Room>.Filter.Eq(r => r.Id, roomId);
+            rooms.DeleteOne(filter);
+        }
+        public List<Room> GetAllRooms()
+        {
+            var rooms = _repository.GetCollection<Room>("rooms");
+            return rooms.Find(new BsonDocument()).ToList();
+        }
+        public Room GetRoomById(ObjectId roomId)
+        {
+            var rooms = _repository.GetCollection<Room>("rooms");
+            var filter = Builders<Room>.Filter.Eq(r => r.Id, roomId);
+            return rooms.Find(filter).FirstOrDefault();
+        }
+        public List<Room> GetRoomsByStatus(string status)
+        {
+            var rooms = _repository.GetCollection<Room>("rooms");
+            var filter = Builders<Room>.Filter.Eq(r => r.Status, status);
+            return rooms.Find(filter).ToList();
+        }
+
     }
 }
